981508b89f1203ffffa4449c16c058b56f45fcb8
nobody <nobody@example.com>
2022-09-10 00:00:00 +0000

    Change String formatter to indent lines

--- fmt_string.go
+++ fmt_string.go
@@ -1,11 +1,21 @@
 package structformat
 
+import (
+	"bufio"
+	"strings"
+)
+
 type StringFormatter []string
 
 var _ Formatter = (*StringFormatter)(nil)
 
-func String(strings ...string) StringFormatter {
-	return StringFormatter(strings)
+func String(s string) StringFormatter {
+	var lines []string
+	sc := bufio.NewScanner(strings.NewReader(s))
+	for sc.Scan() {
+		lines = append(lines, sc.Text())
+	}
+	return StringFormatter(lines)
 }
 
 func (f StringFormatter) StructFormat() (ret NestedLines) {
